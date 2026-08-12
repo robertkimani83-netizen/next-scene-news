7bde65d202d45234707308ba0d0d341925716664
robertkimani83-netizen <robertkimani83@gmail.com>
2026-08-12 22:53:35 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "Flash254 – Kenya News, As It Happens",
-description: "AI-curated Kenyan news, updated around the clock.",
+  title: "VOX254 - The Voice of 254",
+  description: "AI-curated Kenyan news, updated around the clock.",
   verification: {
-    google: "ZTZTto1zIxSwEeQOPpfU6zHQs02iqqY-m14a3sDOfNU",
+    google: "ZTZTto1zIxSwEeQOPpFU6zHQs02iqqY-m14a3sDOfNU",
   },
 };
 
@@ -12,7 +12,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7244574512843335"
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7244574512843335"
           crossOrigin="anonymous"></script>
       </head>
       <body>{children}</body>
